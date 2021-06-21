813f00046a31db34ec14f029656742f9565956b6
rizzet97 <83186182+rizzet97@users.noreply.github.com>
2021-06-21 18:37:02 +0200

    Initial commit

---
+++ database.hpp
@@ -0,0 +1,13 @@
+/*
+    Przechowywanie rekordów studentów o strukturze: Imię, nazwisko, adres, nr indeksu, PESEL, płeć
+    Dodawanie nowych studentów
+    Wyświetlanie całej bazy danych
+    Wyszukiwanie po nazwisku
+    Wyszukiwanie po numerze PESEL
+    Sortowanie po numerze PESEL
+    Sortowanie po nazwisku
+    Usuwanie po numerze indeksu
+*/
+
+#pragma once
+#include <iostream>
---
+++ main.cpp
@@ -0,0 +1,16 @@
+/*
+    Przechowywanie rekordów studentów o strukturze: Imię, nazwisko, adres, nr indeksu, PESEL, płeć
+    Dodawanie nowych studentów
+    Wyświetlanie całej bazy danych
+    Wyszukiwanie po nazwisku
+    Wyszukiwanie po numerze PESEL
+    Sortowanie po numerze PESEL
+    Sortowanie po nazwisku
+    Usuwanie po numerze indeksu
+*/
+#include <iostream>
+
+int main() {
+    std::cout << "Hello world!\n";
+    return 0;
+}

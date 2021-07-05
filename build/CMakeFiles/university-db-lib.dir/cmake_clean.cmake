1be33812c22cddfd068b4de35cdd1395af3402ea
rizzet97 <nit.pawel@gmail.com>
2021-07-05 15:17:25 +0200

    build directory added

---
+++ build/CMakeFiles/university-db-lib.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/university-db-lib.dir/database.cpp.o"
+  "libuniversity-db-lib.a"
+  "libuniversity-db-lib.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/university-db-lib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
